d0c251ca6d4d2b548765a26bdb577e2f73c142f4
Alexander <mps10722@gmail.com>
2021-06-15 00:23:59 +0300

    NumPy

--- numpy_ex.py
+++ numpy_ex.py
@@ -1,13 +1,8 @@
 import numpy as np
 
-A1 = np.array((-1, 2))
-A2 = np.array((2, 3))
-A3 = np.array((5, 4))
+i = 5
+Z = np.array([[0, 1, 2, 3],
+              [4, 5, 6, 7],
+              [8, 9, 10, 11]])
 
-a = np.linalg.norm(A1 - A2)
-b = np.linalg.norm(A1 - A3)
-c = np.linalg.norm(A2 - A3)
-s = (a + b + c) / 2
-a = np.sqrt(s * (s - a) * (s - b) * (s - c))
-
-print(round(a, 4))
+print(np.unravel_index(i, Z.shape))
