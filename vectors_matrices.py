f772d67828a38f5a305b9af19cdc0a8f61435052
Alexander <mps10722@gmail.com>
2021-06-20 23:44:36 +0300

    Pandas

--- vectors_matrices.py
+++ vectors_matrices.py
@@ -1,12 +1,15 @@
 import numpy as np
 import pandas as pd
 
-col = "animal"
-row = "e"
-df = pd.DataFrame({'animal': ['cat', 'cat', 'snake', 'dog', 'dog', 'cat', 'snake', 'cat', 'dog', 'dog'],
+df = pd.DataFrame({'animal': ['cat', 'cat', 'snake', 'dog', 'dog', 'cat',
+                              'snake', 'cat', 'dog', 'dog'],
                    'age': [2.5, 3, 0.5, np.nan, 5, 2, 4.5, np.nan, 7, 3],
+                   'name': ['Murzik', 'Pushok', 'Kaa', 'Bobik', 'Strelka',
+                            'Vaska', 'Kaa2', 'Murka', 'Graf', 'Muhtar'],
                    'visits': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
-                   'priority': ['yes', 'yes', 'no', 'yes', 'no', 'no', 'no', 'yes', 'no', 'no']},
-                  index = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'])
+                   'priority': ['yes', 'yes', 'no', 'yes', 'no', 'no', 'no',
+                                'yes', 'no', 'no']},
+                  index=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'])
+group_by = "animal"
 
-print(df['age'].count() * 1.0, df['age'].quantile(0.75), sep='\n')
+print(df.groupby(group_by)['age'].mean())
